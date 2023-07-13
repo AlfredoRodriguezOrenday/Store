a017626a7e3c1e612d68fbb26f2c3e7f58128e30
AprendidendoGithub <R181308546@utna.edu.mx>
2023-07-12 21:16:08 -0500

    Finished the documentation of the class DeliveryControl || Se finalizo la documentacion de la clase DeliveryControl

--- Control/DeliveryControl.cs
+++ Control/DeliveryControl.cs
@@ -9,9 +9,14 @@ using System.Data;
 
 namespace CRUDSTORE.Control
 {
+    /// <summary>
+    /// This class is used to do different actions like Create, Read, Update and Delete. 
+    /// </summary>
     internal class DeliveryControl
     {
+        /// <value>The propiety ConnectionDB is used to access his methods. </value>
         private Connection ConnectionDB;
+        /// <value> The property Query is used to create Querys. </value>
         SqlCommand Query;
 
         public DeliveryControl()
@@ -19,9 +24,9 @@ namespace CRUDSTORE.Control
             ConnectionDB = new Connection();
         }
         /// <summary>
-        /// 
+        /// This function is used to save register in the database.
         /// </summary>
-        /// <param name="DeliveryProducts"></param>
+        /// <param name="DeliveryProducts">It's an object of kind DeliveryProducts that have the parameters to save.</param>
         public void AddDeliveryProduct(DeliveryProduct DeliveryProducts)
         {
             Query = new SqlCommand("Exec SP_SaveDeliveryProducts @DeliveryName, @Company, @PhoneNumber, @DeliveryDays");
@@ -32,23 +37,37 @@ namespace CRUDSTORE.Control
             ConnectionDB.ExecuteQueryWithOutReturnData(Query);
         }
 
-        public void UpdateDeliveryProduct(DeliveryProduct DP)
+        /// <summary>
+        /// This function is used to update a register in the database.
+        /// </summary>
+        /// <param name="DeliveryProducts">It's an object of kind DeliveryProducts that have the parameters to update a register.</param>
+        public void UpdateDeliveryProduct(DeliveryProduct DeliveryProducts)
         {
             Query = new SqlCommand("Exec SP_UpdateDeliveryProducts @ID, @DeliveryName, @Company, @PhoneNumber, @DeliveryDays");
-            Query.Parameters.Add("@ID", SqlDbType.Int).Value = DP.ID;
-            Query.Parameters.Add("@DeliveryName", SqlDbType.VarChar).Value = DP.Name;
-            Query.Parameters.Add("@Company", SqlDbType.VarChar).Value = DP.Company;
-            Query.Parameters.Add("@PhoneNumber", SqlDbType.Int).Value = DP.PhoneNumber;
-            Query.Parameters.Add("@DeliveryDays", SqlDbType.VarChar).Value = DP.DeliveryDays;
+            Query.Parameters.Add("@ID", SqlDbType.Int).Value = DeliveryProducts.ID;
+            Query.Parameters.Add("@DeliveryName", SqlDbType.VarChar).Value = DeliveryProducts.Name;
+            Query.Parameters.Add("@Company", SqlDbType.VarChar).Value = DeliveryProducts.Company;
+            Query.Parameters.Add("@PhoneNumber", SqlDbType.Int).Value = DeliveryProducts.PhoneNumber;
+            Query.Parameters.Add("@DeliveryDays", SqlDbType.VarChar).Value = DeliveryProducts.DeliveryDays;
             ConnectionDB.ExecuteQueryWithOutReturnData(Query);
         }
 
-        public void DeleteDeliveryProduct(DeliveryProduct DP)
+        /// <summary>
+        /// This function is used to delete a register in the database.
+        /// </summary>
+        /// <param name="DeliveryProducts">It's an object of kind DeliveryProducts that have the parameter to delete a register.</param>
+        public void DeleteDeliveryProduct(DeliveryProduct DeliveryProducts)
         {
             Query = new SqlCommand("Exec SP_DeleteDeliveryProducts @ID");
-            Query.Parameters.Add("@ID", SqlDbType.Int).Value = DP.ID;
+            Query.Parameters.Add("@ID", SqlDbType.Int).Value = DeliveryProducts.ID;
             ConnectionDB.ExecuteQueryWithOutReturnData(Query);
         }
+
+        /// <summary>
+        /// This function can do Querys to see information of the database.
+        /// </summary>
+        /// <param name="QueryCommand">It's a string that will be have a Query to return anything.</param>
+        /// <returns></returns>
 
         public DataSet ShowDelivery(string QueryCommand)
         {

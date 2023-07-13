6f4ee763f90b2ef52a769cc563e9deeee99cdf5e
AprendidendoGithub <R181308546@utna.edu.mx>
2023-07-12 22:48:35 -0500

    Finished the documentation of the class MainView || Se finalizo la documentacion de la clase MainView

--- Form1.cs
+++ Form1.cs
@@ -11,27 +11,42 @@ using CRUDSTORE.View;
 
 namespace CRUDSTORE
 {
+    /// <summary>
+    /// This class is used to see the main view.
+    /// </summary>
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// This method redirect to the Delivery view when the user click in the button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnProviders_Click(object sender, EventArgs e)
         {
             DeliveryView DV = new DeliveryView();
             DV.Show();
             this.Hide();
         }
-
+        /// <summary>
+        /// This method redirect to the Products view when the user click in the button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnProducts_Click(object sender, EventArgs e)
         {
             ProductsView PV = new ProductsView();
             PV.Show(); 
             this.Hide();
         }
-
+        /// <summary>
+        /// This method redirect to the Clients view when the user click in the button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnClients_Click(object sender, EventArgs e)
         {
             ClientView CV = new ClientView();
